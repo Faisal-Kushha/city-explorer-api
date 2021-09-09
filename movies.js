1744f829ea0c9d8d4a0c145c938b08976b4d3bbb
Faisal-Kushha <fkushha89@hotmail.com>
2021-09-09 12:57:16 +0300

    Done

--- movies.js
+++ movies.js
@@ -1,26 +1,32 @@
 const axios = require("axios");
-
+let movieMemory = {};
 function getMovies(req, res) {
   let name = req.query.name;
-  let URL = `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false`;
-  axios
-    .get(URL)
-    .then((arr) => {
-      let newMovie = arr.data.results.map((item) => {
-        return new Load(item);
+
+  if (movieMemory[name] !== undefined) {
+    res.send(movieMemory[name]);
+  } else {
+    let URL = `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1&include_adult=false`;
+    axios
+      .get(URL)
+      .then((arr) => {
+        let newMovie = arr.data.results.map((item) => {
+          return new Load(item);
+        });
+        movieMemory[name] = newMovie;
+        res.send(newMovie);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-      res.send(newMovie);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  function Load(day) {
-    (this.title = day.original_title),
-      (this.overview = day.overview),
-      (this.average_votes = day.vote_average),
-      (this.total_votes = day.vote_count),
-      (this.popularity = day.popularity),
-      (this.released_on = day.release_date);
+    function Load(day) {
+      (this.title = day.original_title),
+        (this.overview = day.overview),
+        (this.average_votes = day.vote_average),
+        (this.total_votes = day.vote_count),
+        (this.popularity = day.popularity),
+        (this.released_on = day.release_date);
+    }
   }
 }
 module.exports = getMovies;
